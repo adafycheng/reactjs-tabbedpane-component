530d78b8358e826e647e4408d0a4aae607eef5cb
adafycheng <adafycheng3110@gmail.com>
2021-11-25 16:18:14 +0000

    Published for v1.0.4. 1. Show content of the first item. 2. Change the cursor to pointer.

--- src/index.js
+++ src/index.js
@@ -14,28 +14,33 @@ let navDivStyle = {
 
 const TabbedPaneComponent = ({data}) => {
 
-
-
-    $(function() {
-      // read the input JSON content
-      let contentStr = "";
+  $(function() {
+    // read the input JSON content
+    let contentStr = "";
+    if (data != undefined) {
       for (let i=0; i<data.contents.length; i++) {
         contentStr += data.contents[i].subject;
         let newDiv = $("<div class=" + styles.navbarDiv + "></div>");
-        let newAnchor = $("<a></a>").text(data.contents[i].subject).attr("title", data.contents[i].text);
+        let newAnchor = $("<a class=\"paneLink\"></a>").text(data.contents[i].subject).attr("title", data.contents[i].text);
         newDiv.append(newAnchor);
         newAnchor.click(function(){ $("#paneContent").html( this.title ); });
         $("#navbar").append(newDiv);
-
       }
-    });
+      if (data.contents.length > 0) {
+        // Get the first link and click.
+        console.log("length > 0");
+        $(".paneLink:first").click();
+      }
+    }
 
-    return (
-      <div id="pane" className={styles.pane}>
-        <div id="navbar"></div>
-        <div id="paneContent" className={styles.paneContent}></div>
-      </div>
-    )
+  });
+
+  return (
+    <div id="pane" className={styles.pane}>
+      <div id="navbar"></div>
+      <div id="paneContent" className={styles.paneContent}></div>
+    </div>
+  )
 }
 
 export default TabbedPaneComponent
