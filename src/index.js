ca81e8de40da80fa9faf6880fd0bcfa46d1ac040
Ada Cheng <adafycheng2020@gmail.com>
2021-12-23 21:27:50 +0000

    Published for v1.0.7. 1. Fix problems by running npm ci.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from './styles.module.scss'
 import $ from 'jquery'
 
-
 const TabbedPaneComponent = ({ data }) => {
   $(function () {
     // read the input JSON content
